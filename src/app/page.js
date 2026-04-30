3591f024c000f24d70d8d909a926e189f2287876
Wasif Hasan <wasifhasancse@gmail.com>
2026-05-01 00:07:45 +0600

    implement Hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Banner />
     </div>
   );
 }
