c664e9ef2238a60b51a330b08278ba2545420db6
Wasif Hasan <wasifhasancse@gmail.com>
2026-05-01 00:03:01 +0600

    initialize pixgen with navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+//website title: pixgen - AI Image 
+const Navbar = () => {
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            loading="eager"
+            width={30}
+            height={30}
+            className="object-cover h-auto w-auto"
+          />
+          <h3 className="font-black text-lg">pixgen.</h3>
+        </div>
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/all-photos"}>All Photos</Link>
+          </li>
+          <li>
+            <Link href={"/pricing"}>Pricing</Link>
+          </li>
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          <ul className="flex items-center  text-sm">
+            <li>
+              <Link href={"/signup"}>SignUp</Link>
+            </li>
+            <li>
+              <Link href={"/signin"}>SignIn</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
